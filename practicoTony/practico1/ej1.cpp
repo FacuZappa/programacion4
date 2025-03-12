821d5ba914f98aa1c6cb228c7c28416d57005c78
tontony04 <124315007+tontony04@users.noreply.github.com>
2025-03-12 19:10:06 -0300

    Create ej1.cpp

---
+++ practicoTony/practico1/ej1.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+#include <cmath>
+using namespace std;
+
+class segmento {
+    public:
+        punto p1, p2;
+        
+    private:
+        segmento(punto, punto);
+};
+
+
+
+class punto {
+    private:
+        float puntox, puntoy;
+    
+    public:
+        float getX();
+        float getY();
+        void modificarPuntoX(float);
+        void modificarPuntoY(float);
+        void segmento(float, float);
+};
+
+void punto::modificarPuntoX(float x){
+    this->puntox = x;
+}
+void punto::modificarPuntoY(float y){
+    this->puntoy = y;
+}
+float punto::getX(){
+    return this->puntox;
+}
+float punto::getY(){
+    return this->puntoy;
+}
